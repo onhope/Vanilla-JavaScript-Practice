1942f893edc7b5048f9e16c94857c5b314ee0122
onhope <chch4934@gmail.com>
2026-01-05 16:15:07 +0900

    1. ImageDragDrop

---
+++ 1. ImageDragDrop/script.js
@@ -0,0 +1,17 @@
+const boxes = document.querySelectorAll(".box"),
+    image = document.querySelector(".image");
+
+boxes.forEach((box, index) => {
+    box.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        box.classList.add("hovered");
+    })
+    box.addEventListener("dragleave", () => {
+        box.classList.remove("hovered");
+    })
+
+    box.addEventListener("drop", () => {
+        box.appendChild(image);
+        box.classList.remove("hovered");
+    })
+});
